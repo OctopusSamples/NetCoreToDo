4267d9ae63e2fbe9cdb80197618c79e145a29dc1
octopusbob <bob.walker@octopus.com>
2019-11-22 14:39:25 -0600

    Closes #4 - Separating out the name of the environment from the deployment

--- src/Models/AppSettings.cs
+++ src/Models/AppSettings.cs
@@ -8,12 +8,12 @@ namespace OctopusSamples.CosmosDB.QuickStart.Web.Models
     public class AppSettings
     {
         public string AppVersion { get; set; }
-        public string EnvironmentName { get; set; }
+        public string DeploymentEnvironment { get; set; }
         public string AccountEndpoint { get; set; }
         public string AccountKeys { get; set; }
         public string Database { get; set; }
         public string Collection { get; set; }
         public string S3BucketPath { get; set; }
-        public string S3VersionBucketPath => $"{S3BucketPath}/{AppVersion}/{EnvironmentName}";
+        public string S3VersionBucketPath => $"{S3BucketPath}/{AppVersion}/{DeploymentEnvironment}";
     }
 }
